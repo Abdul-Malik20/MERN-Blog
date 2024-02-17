e7e590f56ffcea3c5ab666ea3cd48b65cc4038b0
Abdul-Malik20 <j.abdmalik20@gmail.com>
2024-02-17 16:01:38 +0530

    Dark mode functionality

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { store } from '../src/redux/store.js'
 import { Provider } from 'react-redux'
+import ThemeProvider from './components/ThemeProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
 )
